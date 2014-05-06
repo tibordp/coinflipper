6e3be6942e617b7c362ca31eb0bc808bc301fb8c
Tibor Djurica Potpara <tibor.djurica@ojdip.net>
2014-05-06 12:49:55 -0700

    Formatting - QF

--- coinflipper.cc
+++ coinflipper.cc
@@ -368,22 +368,22 @@ int coin_status(char* server_address) {
 	cout << "Total coins flipped:\t" << dec << commify(cf.total_flips()) << endl;
 	cout << "Coins per second:\t" << dec << commify(cf.flips_per_second()) << endl << endl;
 
-	size_t max = 0;
+	array<size_t, 4> max {0,0,0,0};
 
 	array<string, 127> values;
 
-	for (int i = 0; i < 32; ++i)
+	for (int i = 0; i < 127; ++i)
 	{
 		values[i] = commify(results[i]);
-		max = ::max(max, values[i].size());
+		max[i / 64] = ::max(max[i / 64], values[i].size());
 	}
 
 	for (int i = 0; i < 32; ++i)
 	{
-		cout << dec << setw(3) << i + 0  << ": " << setw(max) << results[i + 0 ] << "  " <<
-					   setw(3) << i + 32 << ": " << setw(max) << results[i + 32] << "  " <<
-					   setw(3) << i + 64 << ": " << setw(max) << results[i + 64] << "  " <<
-					   setw(3) << i + 96 << ": " << setw(max) << results[i + 96] << endl;
+		cout << dec << setw(3) << i + 0  << ": " << setw(max[1]) << values[i + 0 ] << "   " <<
+					   setw(3) << i + 32 << ": " << setw(max[2]) << values[i + 32] << "   " <<
+					   setw(3) << i + 64 << ": " << setw(max[3]) << values[i + 64] << "   " <<
+					   setw(3) << i + 96 << ": " << setw(max[4]) << values[i + 96] << endl;
 	}
 
 	return 0;
