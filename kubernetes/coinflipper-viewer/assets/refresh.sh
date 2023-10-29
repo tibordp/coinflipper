ae746d901d6c8b893becb8b5ea145a22194fc6f4
Tibor Djurica Potpara <tibor.djurica@ojdip.net>
2023-10-29 11:43:31 +0000

    more metrics

--- kubernetes/coinflipper-viewer/assets/refresh.sh
+++ kubernetes/coinflipper-viewer/assets/refresh.sh
@@ -3,12 +3,23 @@ set -e
 
 while true; do
     /coinflipper status coinflipper.coinflipper.svc.cluster.local > /pod-data/~status.txt
-    total_flips=$(grep "Total coins flipped" /pod-data/~status.txt | cut -d ":" -f 2 | sed -e 's/[ ,]//g')
 
-    echo '# HELP coinflipper_total_flips Total number of coins flipped' > /pod-data/~metrics.txt
-    echo '# TYPE coinflipper_total_flips counter' >> /pod-data/~metrics.txt
-    echo "coinflipper_total_flips $total_flips" >> /pod-data/~metrics.txt
+    total_flips=$(grep "Total coins flipped" /pod-data/~status.txt | cut -d ":" -f 2 | sed -e 's/[ ,]//g')
+    connected_clients=$(grep -E " cps$" /pod-data/~status.txt | wc -l)
+
+    if [[ -z "$total_flips" ]]; then
+       rm /pod-data/metrics
+    else
+        echo '# HELP coinflipper_total_flips Total number of coins flipped' > /pod-data/~metrics.txt
+        echo '# TYPE coinflipper_total_flips counter' >> /pod-data/~metrics.txt
+        echo "coinflipper_total_flips $total_flips" >> /pod-data/~metrics.txt
+        echo '' >> /pod-data/~metrics.txt
+        echo '# HELP coinflipper_connected_clients Number of connected clients' >> /pod-data/~metrics.txt
+        echo '# TYPE coinflipper_connected_clients gauge' >> /pod-data/~metrics.txt
+        echo "coinflipper_connected_clients $connected_clients" >> /pod-data/~metrics.txt
+
+        mv /pod-data/~metrics.txt /pod-data/metrics
+    fi
 
     mv /pod-data/~status.txt /pod-data/status.txt
-    mv /pod-data/~metrics.txt /pod-data/metrics
 done
