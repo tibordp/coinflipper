3ccb57e5a415e72de0e8198bce0658794c9ebb4d
Tibor Djurica Potpara <tibor.djurica@ojdip.net>
2023-10-29 11:30:24 +0000

    add service monitor

--- kubernetes/coinflipper-viewer/assets/refresh.sh
+++ kubernetes/coinflipper-viewer/assets/refresh.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+set -e
+
 while true; do
     /coinflipper status coinflipper.coinflipper.svc.cluster.local > /pod-data/~status.txt
+    total_flips=$(grep "Total coins flipped" /pod-data/~status.txt | cut -d ":" -f 2 | sed -e 's/[ ,]//g')
+
+    echo '# HELP coinflipper_total_flips Total number of coins flipped' > /pod-data/~metrics.txt
+    echo '# TYPE coinflipper_total_flips counter' >> /pod-data/~metrics.txt
+    echo "coinflipper_total_flips $total_flips" >> /pod-data/~metrics.txt
+
     mv /pod-data/~status.txt /pod-data/status.txt
+    mv /pod-data/~metrics.txt /pod-data/metrics
 done
